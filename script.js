f4165f56448c01f69c6202f346edb07b7c260cff
Renan Borba <renandbm.rb@gmail.com>
2020-07-14 16:06:58 -0300

    Index and style.css completion + Added script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /** App **/
+
+// Selecionar o elemento do HTmL
+document.querySelector("#items")
+  // "ouvidor de eventos" ("roda" do mouse)
+  // .addEventListener("wheel", function() {}) // dispara a função
+
+  .addEventListener("wheel", event => {
+    if(event.deltaY > 0) {
+      // rolar para (x, y)
+      event.target.scrollBy(300, 0)
+    } else {
+      event.target.scrollBy(-300, 0)
+    }
+  })
